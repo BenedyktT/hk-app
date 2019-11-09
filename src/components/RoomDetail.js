0d2eb38c0265a10c8d41e7e1cacb944a977012e3
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2019-11-09 14:21:57 +0000

    working at redux

--- src/components/RoomDetail.js
+++ src/components/RoomDetail.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
-import { Context, AppProvider } from "./context";
+import { connect } from "react-redux";
+import { getRooms, getRoomsPending } from "../reducers/index";
+import { fetchRooms } from "../actions/roomActions";
 
-export default class RoomDetail extends Component {
+class RoomDetail extends Component {
+	state = {
+		rooms: null
+	};
+	componentDidMount() {
+		this.props.fetchRooms();
+	}
+	shouldComponentRender() {
+		const { pending } = this.props;
+		if (pending === false) {
+			return false;
+		}
+		// more tests
+		return true;
+	}
+	renderRoom() {
+		const { rooms } = this.props;
+		const room = rooms.filter(e => {
+			console.log(e.id === this.props.match.params.id);
+			return e.id == this.props.match.params.id;
+		});
+		console.log(room);
+		return <h1>room</h1>;
+	}
 	render() {
-		return (
-			<AppProvider>
-				<Context>
-					{value => {
-						return <div className="">{console.log(value.rooms)}</div>;
-					}}
-				</Context>
-			</AppProvider>
-		);
+		if (this.shouldComponentRender()) return <h1>PENDING</h1>;
+		return this.renderRoom();
 	}
 }
+
+const mapStateToProps = state => ({
+	rooms: getRooms(state),
+	pending: getRoomsPending(state)
+});
+export default connect(
+	mapStateToProps,
+	{ fetchRooms }
+)(RoomDetail);
--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
 const initialState = {};
 
+const middleware = [thunk];
 const store = createStore(
 	rootReducer,
 	initialState,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	compose(
+		applyMiddleware(...middleware),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 );
 
 export default store;
