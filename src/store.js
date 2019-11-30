d22abd306b6756b5be6a03ca1bf24c818afe5fba
Benedykt Tyminski <benedykt.tyminski@gmail.com>
2019-11-30 00:13:34 +0000

    removed redux devtools

--- src/store.js
+++ src/store.js
@@ -8,19 +8,19 @@ import "firebase/firestore";
 import { createFirestoreInstance } from "redux-firestore";
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_KEY,
-	authDomain: "hkapp-257620.firebaseapp.com",
-	databaseURL: "https://hkapp-257620.firebaseio.com",
-	projectId: "hkapp-257620",
-	storageBucket: "hkapp-257620.appspot.com",
-	messagingSenderId: "257818188983",
-	appId: "1:257818188983:web:26a1e75a990e68c83f8132",
-	measurementId: "G-9TCRCQH4FK"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "hkapp-257620.firebaseapp.com",
+  databaseURL: "https://hkapp-257620.firebaseio.com",
+  projectId: "hkapp-257620",
+  storageBucket: "hkapp-257620.appspot.com",
+  messagingSenderId: "257818188983",
+  appId: "1:257818188983:web:26a1e75a990e68c83f8132",
+  measurementId: "G-9TCRCQH4FK"
 };
 
 const rrfConfig = {
-	userProfile: "users",
-	useFirestoreForProfile: true
+  userProfile: "users",
+  useFirestoreForProfile: true
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -30,19 +30,19 @@ const initialState = {};
 
 const middleware = [thunk];
 const store = createStore(
-	rootReducer,
-	initialState,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware)
+    /* 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() */
+  )
 );
 
 export const rrfProps = {
-	firebase,
-	config: rrfConfig,
-	dispatch: store.dispatch,
-	createFirestoreInstance
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
 };
 
 export default store;
